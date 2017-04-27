a45597042fd27dfeffe97c503970a8595c4b59e7
alienturnedhuman <alienturnedhuman@googlemail.com>
2017-04-27 16:39:29 +0100

    Now in pink

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +12,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Scene scene = new Scene(root, 1280, 720, Color.MAGENTA);
+
         primaryStage.setTitle("Study Planner");
-        primaryStage.setScene(new Scene(root, 1280, 720));
+
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
