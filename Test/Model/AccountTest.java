433bc69578633e188e9915904c6cdfb7d44b8d66
bijan <Bijan.ghasemiafshar@gmail.com>
2017-05-04 12:58:48 +0100

    added the first test

---
+++ Test/Model/AccountTest.java
@@ -0,0 +1,46 @@
+package Model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bijan on 04/05/2017.
+ */
+public class AccountTest {
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+
+    @Test
+    public void getStudentDetails() throws Exception {
+        ArrayList<String> personName = new ArrayList<String>();
+        personName.add("John");
+        Person person = new Person(personName, "Andrew", "Priminister", "Andew.apple@applemail.com", true);
+
+        Account account = new Account(person, "buttflock");
+
+        assertEquals(person, account.getStudentDetails());
+    }
+
+    @Test
+    public void getStudentNumber() throws Exception {
+    }
+
+    @Test
+    public void setStudentDetails() throws Exception {
+    }
+
+    @Test
+    public void setStudentNumber() throws Exception {
+    }
+
+}
