b879e4ac01a089fef4474253e5f6747acc677d63
Andrew Odintsov <a1covker@gmail.com>
2017-05-08 21:26:45 +0100

    UI tests for CreateAccount.fxml

--- Test/View/CreateAccountUITest.java
+++ Test/View/CreateAccountUITest.java
@@ -1,11 +1,15 @@
 package View;
 
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import org.junit.Test;
 
 
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.base.NodeMatchers.hasText;
+import static org.testfx.matcher.base.NodeMatchers.isDisabled;
+import static org.testfx.matcher.base.NodeMatchers.isEnabled;
 
 
 /**
@@ -14,14 +18,43 @@ import static org.testfx.matcher.base.NodeMatchers.hasText;
  */
 public class CreateAccountUITest extends FXBase {
 
-final String CREATE_ACCOUT_LABEL = "#createAccountLabel";
+
+    @Test
+    public void testTextField () {
+        TextField t = find("#salutation");
+        TextField t1 = find("#full_name");
+        TextField t3 = find("#account_no");
+        TextField t4 = find("#email");
+        Button b1 = find("#submit");
+
+        clickOn(t).write("Mr");
+        clickOn(t1).write("Andrei Odintsov");
+        clickOn(t3).write("100125464");
+        clickOn(t4).write("a1covker@gmail.com");
 
 
+        verifyThat(b1, isEnabled());
+
+    }
 
 
     @Test
-    public void testLabel () {
-       verifyThat(CREATE_ACCOUT_LABEL, hasText("Create Account"));
+    public void testTextFieldRegex () {
+        TextField t = find("#salutation");
+        TextField t1 = find("#full_name");
+        TextField t3 = find("#account_no");
+        TextField t4 = find("#email");
+        Button b1 = find("#submit");
+
+
+        clickOn(t).write("Mr");
+        clickOn(t1).write("Andrei Odintsov");
+        clickOn(t3).write("100125464");
+        clickOn(t4).write("somethingnotemail");
+
+
+        verifyThat(b1, isDisabled());
+
 
     }
 
