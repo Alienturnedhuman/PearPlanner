178fcd549134f24ec02e200367c731fff213797a
Andrew Odintsov <a1covker@gmail.com>
2017-05-08 23:47:27 +0100

    fixed StudyPlannerController test

--- Test/Model/ModelEntityTest.java
+++ Test/Model/ModelEntityTest.java
@@ -2,6 +2,7 @@ package Model;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import static org.junit.Assert.*;
 /**
  * Created by bijan on 08/05/2017.
  */
+@Ignore
 public abstract class ModelEntityTest {
 
     ModelEntity modelEntity;
