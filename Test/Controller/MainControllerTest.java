721fca88ee9af8933e79cf073ef15e2a21e3582c
bijan <Bijan.ghasemiafshar@gmail.com>
2017-05-13 21:52:29 +0100

    Added ExamTest

--- Test/Controller/MainControllerTest.java
+++ Test/Controller/MainControllerTest.java
@@ -1,16 +1,8 @@
 package Controller;
 
-import View.Main;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Node;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
+
 import javafx.stage.Stage;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
 import static org.junit.Assert.*;
@@ -27,21 +19,13 @@ public class MainControllerTest extends ApplicationTest
         MainController.isNumeric("23");
     }
 
-    @After
-    public void tearDown() throws Exception
-    {
-        FxToolkit.hideStage();
-        release(new KeyCode[]{});
-        release(new MouseButton[]{});
-    }
-
     @Test
     public void isNumeric() throws Exception
     {
+        // Testing a valid number
         assertEquals(true, MainController.isNumeric("23"));
-    }
 
-    public <T extends Node> T find (final String query) {
-        return (T) lookup(query).queryAll().iterator().next();
+        // Testing an invalid number
+        assertEquals(false, MainController.isNumeric("23df"));
     }
 }
--- Test/Model/ExamTest.java
+++ Test/Model/ExamTest.java
@@ -1,56 +1,65 @@
-//package Model;
-//
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.*;
-//
-///**
-// * Created by bijan on 13/05/2017.
-// */
-//public class ExamTest
-//{
-//    Building building;
-//    Room room;
-//    Exam finalexam, examResit;
-//    ExamEvent examEvent;
-//    Person person1, person2;
-//
-//    @Before
-//    public void setUp() throws Exception
-//    {
-//        building = new Building("JSC", 52.6308859, 1.297355);
-//        room = new Room("JSC_1_02", building);
-//        examEvent = new ExamEvent("13/05/2017T14:50:12", room, 120);
-//        person1 = new Person("Dr.", "Mark Fisher", true);
-//        person2 = new Person("Dr.", "Rudy Lapeer", true);
-//        examResit = null;
-//        finalexam = new Exam(30, person1, person1, person2, 100, examEvent, examResit);
-//}
-//
-//    @After
-//    public void tearDown() throws Exception
-//    {
-//        building = null;
-//        room = null;
-//        examEvent = null;
-//        person1 = null;
-//        person2 = null;
-//        examResit = null;
-//        finalexam = null;
-//    }
-//
-//    @Test
-//    public void getResit() throws Exception
-//    {
-//        assertEquals(examResit, finalexam.getResit());
-//    }
-//
-//    @Test
-//    public void getTimeSlot() throws Exception
-//    {
-////        assertEquals(examEvent, finalexam.getTimeSlot());
-//    }
-//
-//}
+package Model;
+
+import Controller.MainController;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bijan on 13/05/2017.
+ */
+public class ExamTest extends ApplicationTest
+{
+    Building building;
+    Room room;
+    Exam finalexam, examResit;
+    ExamEvent examEvent;
+    Person person1, person2;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        MainController.isNumeric("23");
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+        building = new Building("JSC", 52.6308859, 1.297355);
+        room = new Room("JSC_1_02", building);
+        examEvent = new ExamEvent("13/05/2017T14:50:12", room, 120);
+        person1 = new Person("Dr.", "Mark Fisher", true);
+        person2 = new Person("Dr.", "Rudy Lapeer", true);
+        examResit = null;
+        finalexam = new Exam(30, person1, person1, person2, 100, examEvent, examResit);
+}
+
+    @After
+    public void tearDown() throws Exception
+    {
+        building = null;
+        room = null;
+        examEvent = null;
+        person1 = null;
+        person2 = null;
+        examResit = null;
+        finalexam = null;
+    }
+
+    @Test
+    public void getResit() throws Exception
+    {
+        assertEquals(examResit, finalexam.getResit());
+    }
+
+    @Test
+    public void getTimeSlot() throws Exception
+    {
+        assertEquals(examEvent, finalexam.getTimeSlot());
+    }
+
+}
