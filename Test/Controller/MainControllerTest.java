a7d723e1cc2267e80d010d0c46fc7e0cac22fdd3
bijan <Bijan.ghasemiafshar@gmail.com>
2017-05-13 20:51:44 +0100

    Added EventTest & MainControllerTest

--- Test/Controller/MainControllerTest.java
+++ Test/Controller/MainControllerTest.java
@@ -1,42 +1,46 @@
-package Controller;
-
-import View.Main;
-import javafx.application.Application;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
-import javafx.stage.Stage;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.testfx.api.FxToolkit;
-import org.testfx.framework.junit.ApplicationTest;
-
-import static org.junit.Assert.*;
-
-/**
- * Created by bijan on 13/05/2017.
- */
-public class MainControllerTest extends ApplicationTest
-{
-
-    @Override
-    public void start(Stage primaryStage) throws Exception
-    {
-        MainController.initialise();
-    }
-
-    @After
-    public void tearDown() throws Exception
-    {
-        FxToolkit.hideStage();
-        release(new KeyCode[]{});
-        release(new MouseButton[]{});
-    }
-
-    @Test
-    public void isNumeric() throws Exception
-    {
-        assertEquals(true, MainController.isNumeric("23"));
-    }
-
-}
+//package Controller;
+//
+//import View.Main;
+//import javafx.application.Application;
+//import javafx.scene.Node;
+//import javafx.scene.input.KeyCode;
+//import javafx.scene.input.MouseButton;
+//import javafx.stage.Stage;
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.testfx.api.FxToolkit;
+//import org.testfx.framework.junit.ApplicationTest;
+//
+//import static org.junit.Assert.*;
+//
+///**
+// * Created by bijan on 13/05/2017.
+// */
+//public class MainControllerTest extends ApplicationTest
+//{
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception
+//    {
+//        MainController.initialise();
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception
+//    {
+//        FxToolkit.hideStage();
+//        release(new KeyCode[]{});
+//        release(new MouseButton[]{});
+//    }
+//
+//    @Test
+//    public void isNumeric() throws Exception
+//    {
+//        assertEquals(true, MainController.isNumeric("23"));
+//    }
+//
+//    public <T extends Node> T find (final String query) {
+//        return (T) lookup(query).queryAll().iterator().next();
+//    }
+//}
