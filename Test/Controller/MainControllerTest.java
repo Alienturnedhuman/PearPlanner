b512eef6924d6f3470a68f916689404c890567ec
bijan <Bijan.ghasemiafshar@gmail.com>
2017-05-13 20:20:55 +0100

    Added EventTest & MainControllerTest

--- Test/Controller/MainControllerTest.java
+++ Test/Controller/MainControllerTest.java
@@ -2,10 +2,13 @@ package Controller;
 
 import View.Main;
 import javafx.application.Application;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
 import static org.junit.Assert.*;
@@ -22,6 +25,13 @@ public class MainControllerTest extends ApplicationTest
         MainController.initialise();
     }
 
+    @After
+    public void tearDown() throws Exception
+    {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
 
     @Test
     public void isNumeric() throws Exception
