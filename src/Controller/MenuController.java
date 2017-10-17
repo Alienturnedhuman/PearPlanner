5597231ba398691f26fd3152c41f4159ab20bf0a
RachelHill8 <hill.382@wright.edu>
2017-10-17 13:49:43 -0400

    Fixes mainContent sizing issue

--- src/Controller/MenuController.java
+++ src/Controller/MenuController.java
@@ -221,6 +221,7 @@ public class MenuController implements Initializable {
 		calendar.setTooltip(new Tooltip("Open CALENDAR"));
 
 		// Update main pane:
+
 		this.mainContent.getChildren().remove(1, this.mainContent.getChildren().size());
 		this.topBox.getChildren().clear();
 		this.topBox.getChildren().add(this.welcome);
@@ -233,29 +234,23 @@ public class MenuController implements Initializable {
 		Label studyProfile = new Label(profile.getName());
 		studyProfile.getStyleClass().add("title");
 		GridPane.setMargin(studyProfile, new Insets(10));
-		this.mainContent.addRow(1, studyProfile);
-		this.mainContent.setMaxSize(Control.USE_COMPUTED_SIZE, 1000);
 		this.mainContent.getColumnConstraints().add(
 				new ColumnConstraints(
 						Control.USE_COMPUTED_SIZE,
-						Double.POSITIVE_INFINITY,
-						2000,
+						Control.USE_COMPUTED_SIZE,
+						Control.USE_COMPUTED_SIZE,
 						Priority.ALWAYS,
 						HPos.CENTER,
 						true));
+
+
 		GridPane modules = new GridPane();
 		modules.setHgap(30);
 		modules.setVgap(20);
-		// This code will be added when wanting to resize the modules to grow with the page
-		//modules.getRowConstraints().add(
-		//		new RowConstraints(
-		//				1,
-		//				Control.USE_COMPUTED_SIZE,
-		//				200,
-		//				Priority.ALWAYS,
-		//				VPos.CENTER,
-		//				true));
-		int colIdx = 0;
+		GridPane.setColumnSpan(studyProfile, GridPane.REMAINING);
+		modules.addRow(1, studyProfile);
+
+		int colIdx = -1;
 		for (Module module : profile.getModules()) {
 			VBox vbox = new VBox();
 			vbox.setSpacing(5);
@@ -285,9 +280,9 @@ public class MenuController implements Initializable {
 			modules.setMaxSize(800, 1000);
 			modules.getColumnConstraints().add(
 					new ColumnConstraints(
+							200,
 							Control.USE_COMPUTED_SIZE,
-							Double.POSITIVE_INFINITY,
-							1000,
+							50000,
 							Priority.ALWAYS,
 							HPos.CENTER,
 							true));
@@ -394,6 +389,7 @@ public class MenuController implements Initializable {
 
 		this.mainContent.addRow(2, table);
 		this.mainContent.getStyleClass().add("list-item");
+		GridPane.setColumnSpan(table, GridPane.REMAINING);
 
 		// Actions toolbar:
 		HBox actions = new HBox();
@@ -461,6 +457,7 @@ public class MenuController implements Initializable {
 		// Layout:
 		VBox layout = new VBox();
 		GridPane.setHgrow(layout, Priority.ALWAYS);
+		GridPane.setColumnSpan(layout, GridPane.REMAINING);
 		layout.setSpacing(10);
 		layout.setPadding(new Insets(15));
 		layout.getStylesheets().add("/Content/stylesheet.css");
@@ -564,6 +561,7 @@ public class MenuController implements Initializable {
 		Label profiles = new Label("Study PROFILES");
 		profiles.getStyleClass().add("title");
 		this.mainContent.addRow(1, profiles);
+		GridPane.setColumnSpan(profiles, GridPane.REMAINING);
 		// =================
 
 		// Columns:
@@ -623,6 +621,7 @@ public class MenuController implements Initializable {
 		// =================
 
 		this.mainContent.addRow(2, table);
+		GridPane.setColumnSpan(table, GridPane.REMAINING);
 		this.mainContent.getStyleClass().add("list-item");
 	}
 
@@ -640,6 +639,7 @@ public class MenuController implements Initializable {
 		Label modules = new Label("MODULES");
 		modules.getStyleClass().add("title");
 		this.mainContent.addRow(1, modules);
+		GridPane.setColumnSpan(modules, GridPane.REMAINING);
 		// =================
 
 		// Columns:
@@ -690,6 +690,7 @@ public class MenuController implements Initializable {
 		// =================
 
 		this.mainContent.addRow(2, table);
+		GridPane.setColumnSpan(table, GridPane.REMAINING);
 		this.mainContent.getStyleClass().add("list-item");
 	}
 
@@ -721,6 +722,7 @@ public class MenuController implements Initializable {
 				details);
 		GridPane.setVgrow(detailsBox, Priority.SOMETIMES);
 		GridPane.setHgrow(detailsBox, Priority.ALWAYS);
+		GridPane.setColumnSpan(detailsBox, GridPane.REMAINING);
 		// =================
 
 		mainContent.addRow(2, detailsBox);
@@ -785,6 +787,7 @@ public class MenuController implements Initializable {
 		// =================
 
 		this.mainContent.addRow(3, moduleContent);
+		GridPane.setColumnSpan(moduleContent, GridPane.REMAINING);
 	}
 
 	/**
@@ -805,6 +808,7 @@ public class MenuController implements Initializable {
 		Label assignments = new Label(assignment.getName());
 		assignments.getStyleClass().add("title");
 		this.mainContent.addRow(1, assignments);
+		GridPane.setColumnSpan(assignments, GridPane.REMAINING);
 		// =================
 
 		// Ganttish chart button:
@@ -837,11 +841,13 @@ public class MenuController implements Initializable {
 		// =================
 
 		mainContent.addRow(2, detailsBox);
+		GridPane.setColumnSpan(detailsBox, GridPane.REMAINING);
 
 		// Content pane:
 		GridPane content = new GridPane();
 		GridPane.setVgrow(content, Priority.ALWAYS);
 		GridPane.setHgrow(content, Priority.ALWAYS);
+		GridPane.setColumnSpan(content, GridPane.REMAINING);
 		content.setVgap(5);
 		// =================
 
@@ -1076,6 +1082,7 @@ public class MenuController implements Initializable {
 		content.add(actionsTask, 1, 1);
 
 		this.mainContent.addRow(3, content);
+		GridPane.setColumnSpan(content, GridPane.REMAINING);
 	}
 
 	/**
