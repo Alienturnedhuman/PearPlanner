69b7edcd0c77208f5155bcc6827bbf33c2148036
Aashishkebab <aashish.bharadwaj01@gmail.com>
2017-10-17 22:53:35 -0400

    Change window sizing semantics
    Change the semantics of the parameters for the scene size, so that it is easier to understand what the functions are doing.

--- src/View/UIManager.java
+++ src/View/UIManager.java
@@ -121,10 +121,10 @@ public class UIManager {
 		loader.setController(UIManager.mc);
 		Parent root = loader.load();
 
-		// Set the scene:
+		// Set the scene, with dimensions based on the screen resolution.
 		mainStage.setScene(new Scene(root,
-				Screen.getPrimary().getVisualBounds().getWidth() / 1.3,
-				Screen.getPrimary().getVisualBounds().getHeight() / 1.1,
+				Screen.getPrimary().getVisualBounds().getWidth() * 0.77,
+				Screen.getPrimary().getVisualBounds().getHeight() * 0.9,
 				true, SceneAntialiasing.BALANCED));
 		mainStage.setTitle("RaiderPlanner");
 		
