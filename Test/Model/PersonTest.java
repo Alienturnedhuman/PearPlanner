1c904d8072fc19915addb05a6f5fb3efae4d848d
matthew-barrett <barrett.77@wright.edu>
2017-10-24 17:46:32 -0400

    Update merged PersonTest class to resolve introduced CheckStyle errors

--- Test/Model/PersonTest.java
+++ Test/Model/PersonTest.java
@@ -21,10 +21,13 @@
 
 package Model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
