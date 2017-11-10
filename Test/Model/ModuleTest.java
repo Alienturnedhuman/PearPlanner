58c0cafcd5b9202712c434e5203e563d21f1957d
matthew-barrett <barrett.77@wright.edu>
2017-11-10 14:42:51 -0500

    Updated ModuleTest class to follow Checkstyle syntax rules.

--- Test/Model/ModuleTest.java
+++ Test/Model/ModuleTest.java
@@ -28,39 +28,32 @@ import org.junit.jupiter.api.Test;
  */
 public class ModuleTest {
 
-    @Test
-    public void replace() throws Exception
-    {
-    }
+	@Test
+	public void replace() throws Exception {
+	}
 
-    @Test
-    public void addAssignment() throws Exception
-    {
-    }
+	@Test
+	public void addAssignment() throws Exception {
+	}
 
-    @Test
-    public void removeAssignment() throws Exception
-    {
-    }
+	@Test
+	public void removeAssignment() throws Exception {
+	}
 
-    @Test
-    public void setOrganiser() throws Exception
-    {
-    }
+	@Test
+	public void setOrganiser() throws Exception {
+	}
 
-    @Test
-    public void setModuleCode() throws Exception
-    {
-    }
+	@Test
+	public void setModuleCode() throws Exception {
+	}
 
-    @Test
-    public void addTimetableEvent() throws Exception
-    {
-    }
+	@Test
+	public void addTimetableEvent() throws Exception {
+	}
 
-    @Test
-    public void removeTimetableEvent() throws Exception
-    {
-    }
+	@Test
+	public void removeTimetableEvent() throws Exception {
+	}
 
 }
