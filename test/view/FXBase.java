1d799c061ac8aba5f4754eba17af2a4fe2ddb9b0
Hayden <richter.20@wright.edu>
2017-11-18 17:22:31 -0500

    CheckStyle issue ref #9

--- test/view/FXBase.java
+++ test/view/FXBase.java
@@ -45,37 +45,37 @@ public abstract class FXBase extends ApplicationTest {
 
 
 
-    @Override
-    public void start(Stage stage) throws Exception
-    {
+	@Override
+	public void start(Stage stage) throws Exception
+	{
 
-        AccountController accountControl = new AccountController();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateAccount.fxml"));
-        loader.setController(accountControl);
-        Parent root = loader.load();
+		AccountController accountControl = new AccountController();
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateAccount.fxml"));
+		loader.setController(accountControl);
+		Parent root = loader.load();
 
-        stage.setOnCloseRequest(e -> {
-            Platform.exit();
-            System.exit(0);
-        });
+		stage.setOnCloseRequest(e -> {
+			Platform.exit();
+			System.exit(0);
+		});
 
-        Scene scene = new Scene(root, 550, 232);
+		Scene scene = new Scene(root, 550, 232);
 
-        stage.setScene(scene);
+		stage.setScene(scene);
 
-        stage.show();
-    }
+		stage.show();
+	}
 
-    @After
-    public void afterEachTest() throws TimeoutException
-    {
-       FxToolkit.hideStage();
-       release(new KeyCode[]{});
-       release(new MouseButton[]{});
-    }
+	@After
+	public void afterEachTest() throws TimeoutException
+	{
+	   FxToolkit.hideStage();
+	   release(new KeyCode[]{});
+	   release(new MouseButton[]{});
+	}
 
 
-    public <T extends Node> T find (final String query) {
-        return (T) lookup(query).queryAll().iterator().next();
-    }
+	public <T extends Node> T find (final String query) {
+		return (T) lookup(query).queryAll().iterator().next();
+	}
 }
