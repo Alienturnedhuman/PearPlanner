edc455c26904aa4299a728a50416d1c5578bf07a
champ <champ@Terrill-Laptop>
2018-02-03 09:47:50 -0500

    Removed module integration from settings controller.

--- src/edu/wright/cs/raiderplanner/controller/SettingsController.java
+++ src/edu/wright/cs/raiderplanner/controller/SettingsController.java
@@ -206,14 +206,9 @@ public class SettingsController implements Initializable {
 
 		initialLoad = false;
 
-		//When user chooses different option in menu
-		//		calendarOpen changes to monitor status within main window.
 		switch (this.current) {
 		case ABOUT: {
-			if (MainController.getSpc().getPlanner().getCurrentStudyProfile() != null) {
-				this.loadAbout();
-			}
-			loadAboutText();
+			loadAbout();
 			break;
 		}
 		case GENERAL: {
@@ -238,262 +233,17 @@ public class SettingsController implements Initializable {
 	}
 
 	/**
-	 * Display the Study ABOUT pane. Loads at settings startup.
+	 * Display About.
 	 * This also initializes aspects of the settings scene.
+	 * Created: 2/3/2018
+	 * Author: Clayton D. Terrill
 	 */
 	public void loadAbout() {
 		// set ToolTips
-
-		StudyProfile profile = MainController.getSpc().getPlanner().getCurrentStudyProfile();
-
-		FlowPane modulesPane = new FlowPane();
-
-		Thread renderModules = new Thread(() -> {
-			Label oldLabel = new Label(this.welcome.getText());
-			Thread sayLoading = new Thread(() -> {
-				this.welcome.setText(this.welcome.getText() + "  LOADING...");
-			});
-			Platform.runLater(sayLoading);
-
-			for (Module module : profile.getModules()) {
-				VBox vbox = new VBox();
-
-				// Set the width of the module to 15% of the screen resolution
-				if (screenWidth > screenHeight) {
-					vbox.setPrefWidth(screenWidth * 0.14);
-				} else {
-					// If device is in portrait mode, set vbox width based on height
-					vbox.setPrefWidth(screenHeight * 0.14);
-				}
-				// Set the height of the module to 112% of its width
-				vbox.setPrefHeight(vbox.getPrefWidth() * 1.12);
-				// Set margin between text and badge to 10% vbox width
-				vbox.setSpacing(vbox.getPrefWidth() * 0.1);
-
-				vbox.setAlignment(Pos.CENTER);
-				vbox.setCursor(Cursor.HAND);
-
-				Label nameLabel = new Label(module.getName());
-				nameLabel.setTextAlignment(TextAlignment.CENTER);
-
-				// Set left margin for title, which creates padding in case title is very long
-				VBox.setMargin(nameLabel, new Insets(0, 0, 0, vbox.getPrefWidth() * 0.04));
-
-				vbox.getChildren().add(nameLabel);
-
-				BufferedImage buff = GanttishDiagram.getBadge(module.calculateProgress(), true, 1);
-				Image image = SwingFXUtils.toFXImage(buff, null);
-				Pane badge = new Pane();
-
-				// Set the distance from left edge to badge 17% of vbox width
-				VBox.setMargin(badge, new Insets(0, 0, 0, vbox.getPrefWidth() * 0.17));
-				// Set the badge width to 66% that of vbox
-				badge.setPrefHeight(vbox.getPrefWidth() * 0.66);
-
-				badge.setBackground(new Background(new BackgroundImage(image,
-						BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
-						BackgroundPosition.DEFAULT, new BackgroundSize(BackgroundSize.AUTO,
-								BackgroundSize.AUTO, false, false, true, false))));
-				vbox.getChildren().add(badge);
-
-				/*
-				 * If mouse clicks on module, depress it. If mouse leaves module while depressed,
-				 * undepress button. If mouse re-enters, then re-depress module. If mouse is not
-				 * depressed when it enters module, show hover effect.
-				 */
-				vbox.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> {
-					if (mouseDown) {
-						vbox.setEffect(this.modulePressedShadow);
-					} else {
-						vbox.setEffect(this.moduleHoverShadow);
-					}
-				});
-				vbox.addEventHandler(MouseEvent.MOUSE_EXITED, e -> {
-					vbox.setEffect(this.moduleDefaultShadow);
-				});
-				vbox.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
-					if (e.getButton() == MouseButton.PRIMARY) {
-						vbox.setEffect(this.modulePressedShadow);
-						mouseDown = true;
-					}
-				});
-				vbox.addEventHandler(MouseEvent.MOUSE_RELEASED, e -> {
-					if (e.getButton() == MouseButton.PRIMARY) {
-						vbox.setEffect(this.moduleDefaultShadow);
-						mouseDown = false;
-					}
-				});
-				vbox.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
-					if (e.getButton() == MouseButton.PRIMARY) {
-						//module.open(this.current);
-					}
-				});
-				vbox.setOnTouchPressed(new EventHandler<TouchEvent>() {
-					@Override
-					public void handle(TouchEvent event) {
-						vbox.setEffect(modulePressedShadow);
-					}
-				});
-				vbox.setOnTouchReleased(new EventHandler<TouchEvent>() {
-					@Override
-					public void handle(TouchEvent event) {
-						vbox.setEffect(moduleDefaultShadow);
-					}
-				});
-
-				vbox.setEffect(this.moduleDefaultShadow);
-				vbox.setStyle("-fx-background-color: white");
-
-				Thread addModule = new Thread(() -> {
-					modulesPane.getChildren().add(vbox);
-				});
-				Platform.runLater(addModule);
-
-				// Ensure shadows don't overlap with edge of FlowPane
-				FlowPane.setMargin(vbox, new Insets(screenHeight * 0.033, 0, screenHeight * 0.022,
-						screenWidth * 0.037));
-			}
-			Thread removeLoading = new Thread(() -> {
-				this.welcome.setText(oldLabel.getText());
-			});
-			Platform.runLater(removeLoading);
-		});
-		renderModules.start();
-
-		/*
-		 * Allow modules to be scrollable if window is too small to display them all on screen
-		 * simultaneously.
-		ScrollPane moduleBox = new ScrollPane();
-		moduleBox.setContent(modulesPane);
-		moduleBox.setStyle("-fx-background-color: transparent");
-		moduleBox.setFitToHeight(true);
-		moduleBox.setFitToWidth(true);
-		GridPane.setColumnSpan(moduleBox, GridPane.REMAINING);
-		this.mainContent.addRow(2, moduleBox);
-		 */
-
-		// Create a details pane:
-	}
-
-	/**
-	 * Display the Study ABOUT pane.
-	 * NOT IMPLEMENTED
-	 */
-	public void loadGeneral() {
-		// Update main pane:
-		this.mainContent.getChildren().remove(1, this.mainContent.getChildren().size());
-		this.topBox.getChildren().clear();
-		this.welcome.setText("GENERAL");
-		this.topBox.getChildren().add(this.welcome);
-		this.title.setText("General Settings");
-
-		/*
-		VBox detailsBox = new VBox(5);
-		Label details = new Label("Testing details label");
-		details.setWrapText(true);
-		detailsBox.getChildren().addAll(new Label("The top line is right here."),
-				new Label("Second sucks"),
-				new Label("Third time is a charm."),
-				new Label("Shake my head, this is fourth."),
-				new Label("This is the last one, I swear."),
-				details);
-		GridPane.setVgrow(detailsBox, Priority.SOMETIMES);
-		GridPane.setHgrow(detailsBox, Priority.ALWAYS);
-
-		this.mainContent.addRow(2, detailsBox);
-		GridPane.setColumnSpan(detailsBox, GridPane.REMAINING);
-		*/
-	}
-
-	/**
-	 * Display the Study ABOUT pane.
-	 * NOT IMPLEMENTED
-	 */
-	public void loadAccount() {
-		// Update main pane:
-		this.mainContent.getChildren().remove(1, this.mainContent.getChildren().size());
-		this.topBox.getChildren().clear();
-		this.welcome.setText("ACCOUNT");
-		this.topBox.getChildren().add(this.welcome);
-		this.title.setText("Account Settings");
-	}
-
-	/**
-	 * Display the Study ABOUT pane.
-	 * NOT IMPLEMENTED
-	 */
-	public void loadTheme() {
-		// Update main pane:
-		this.mainContent.getChildren().remove(1, this.mainContent.getChildren().size());
-		this.topBox.getChildren().clear();
-		this.welcome.setText("THEME");
-		this.topBox.getChildren().add(this.welcome);
-		this.title.setText("Theme Settings");
-	}
-
-	/**
-	 * Display the Study ABOUT pane.
-	 * NOT IMPLEMENTED
-	 */
-	public void loadNotification() {
-		// Update main pane:
-		this.mainContent.getChildren().remove(1, this.mainContent.getChildren().size());
-		this.topBox.getChildren().clear();
-		this.welcome.setText("NOTIFICATION");
-		this.topBox.getChildren().add(this.welcome);
-		this.title.setText("Notification Settings");
-	}
-
-	/**
-	 * Handles the 'Back' Event
-	 * Author: Clayton D. Terrill  1/29/2018
-	 */
-	public void goBack() {
-		initialLoad = true; //Required so class may be reused.
-		MainController.showMain();
-	}
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		this.prepareAnimations();
-		this.isNavOpen = false;
-
-		// Set shadows
-		navList.setEffect(navShadow);
-
-		// Set button actions:
-		this.closeDrawer.setOnAction(e -> openMenu.fire());
-		this.generalBtn.setOnAction(e -> this.main(Window.GENERAL));
-		this.accountBtn.setOnAction(e -> this.main(Window.ACCOUNT));
-		this.themeBtn.setOnAction(e -> this.main(Window.THEME));
-		this.notificationsBtn.setOnAction(e -> this.main(Window.NOTIFICATIONS));
-		this.aboutBtn.setOnAction(e -> this.main(Window.ABOUT));
-
-		// Set nav to close when clicking outside of it
-		this.mainContent.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
-			if (e.getButton() == MouseButton.PRIMARY) {
-				if (this.isNavOpen) {
-					this.openMenu.fire();
-				}
-			}
-		});
-
-		this.welcome = new Label(""); //Not initially needed
-		loadAboutText();
-
-		// Render ABOUT initially:
-		this.main(Window.ABOUT);
-	}
-
-	/**
-	 * Loads the About Menu's Text into the mainContent Pane.
-	 * 2/3/2018
-	 * Author: Clayton D. Terrill
-	 */
-	public void loadAboutText() {
 		this.mainContent.getChildren().remove(1, this.mainContent.getChildren().size());
 		this.topBox.getChildren().clear();
 		this.title.setText("About");
+
 		//Text Labels:
 		Label appName = new Label("RaiderPlanner");
 		appName.setFont(Font.font("Ariel", FontWeight.BOLD , 42));
@@ -549,6 +299,99 @@ public class SettingsController implements Initializable {
 	}
 
 	/**
+	 * Display the general settings.
+	 * NOT IMPLEMENTED
+	 */
+	public void loadGeneral() {
+		// Update main pane:
+		this.mainContent.getChildren().remove(1, this.mainContent.getChildren().size());
+		this.topBox.getChildren().clear();
+		this.welcome.setText("GENERAL");
+		this.topBox.getChildren().add(this.welcome);
+		this.title.setText("General Settings");
+	}
+
+	/**
+	 * Display the general settings.
+	 * NOT IMPLEMENTED
+	 */
+	public void loadAccount() {
+		// Update main pane:
+		this.mainContent.getChildren().remove(1, this.mainContent.getChildren().size());
+		this.topBox.getChildren().clear();
+		this.welcome.setText("ACCOUNT");
+		this.topBox.getChildren().add(this.welcome);
+		this.title.setText("Account Settings");
+	}
+
+	/**
+	 * Display theme settings.
+	 * NOT IMPLEMENTED
+	 */
+	public void loadTheme() {
+		// Update main pane:
+		this.mainContent.getChildren().remove(1, this.mainContent.getChildren().size());
+		this.topBox.getChildren().clear();
+		this.welcome.setText("THEME");
+		this.topBox.getChildren().add(this.welcome);
+		this.title.setText("Theme Settings");
+	}
+
+	/**
+	 * Display the notification settings.
+	 * NOT IMPLEMENTED
+	 */
+	public void loadNotification() {
+		// Update main pane:
+		this.mainContent.getChildren().remove(1, this.mainContent.getChildren().size());
+		this.topBox.getChildren().clear();
+		this.welcome.setText("NOTIFICATION");
+		this.topBox.getChildren().add(this.welcome);
+		this.title.setText("Notification Settings");
+	}
+
+	/**
+	 * Handles the 'Back' Event
+	 * Author: Clayton D. Terrill  1/29/2018
+	 */
+	public void goBack() {
+		initialLoad = true; //Required so class may be reused.
+		MainController.showMain();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		this.prepareAnimations();
+		this.isNavOpen = false;
+
+		// Set shadows
+		navList.setEffect(navShadow);
+
+		// Set button actions:
+		this.closeDrawer.setOnAction(e -> openMenu.fire());
+		this.generalBtn.setOnAction(e -> this.main(Window.GENERAL));
+		this.accountBtn.setOnAction(e -> this.main(Window.ACCOUNT));
+		this.themeBtn.setOnAction(e -> this.main(Window.THEME));
+		this.notificationsBtn.setOnAction(e -> this.main(Window.NOTIFICATIONS));
+		this.aboutBtn.setOnAction(e -> this.main(Window.ABOUT));
+
+		// Set nav to close when clicking outside of it
+		this.mainContent.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
+			if (e.getButton() == MouseButton.PRIMARY) {
+				if (this.isNavOpen) {
+					this.openMenu.fire();
+				}
+			}
+		});
+
+		this.welcome = new Label(""); //Not initially needed
+		loadAbout(); //Using 'About' as the one to show first.
+
+		// Render ABOUT initially:
+		this.main(Window.ABOUT);
+	}
+
+	/**
 	 * Prepares animations for the main window.
 	 */
 	private void prepareAnimations() {
@@ -567,112 +410,4 @@ public class SettingsController implements Initializable {
 			}
 		});
 	}
-
-	/**
-	 * RowFactory for a TableView of Requirement.
-	 *
-	 * @param e1
-	 *			TableView that contains the RowFactory.
-	 *
-	 * @return new RowFactory
-	 */
-	protected static TableRow<Requirement> requirementRowFactory(TableView<Requirement> e1,
-			Assignment assignment) {
-
-		TableRow<Requirement> row = new TableRow<Requirement>() {
-			@Override
-			protected void updateItem(final Requirement item, final boolean empty) {
-				super.updateItem(item, empty);
-				// If completed, mark:
-				if (!empty && item != null) {
-					setText(item.toString());
-					if (item.isComplete()) {
-						this.getStyleClass().add("current-item");
-					}
-				} else {
-					setText(null);
-					this.getStyleClass().remove("current-item");
-				}
-				e1.refresh();
-			}
-		};
-
-		row.setOnMouseClicked(event -> {
-			if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY
-					&& event.getClickCount() == 2) {
-				try {
-					MainController.ui.requirementDetails(row.getItem());
-					e1.refresh();
-				} catch (IOException e) {
-					UiManager.reportError("Unable to open view file");
-				}
-			}
-		});
-
-		row.setOnDragDetected(event -> {
-
-			if (row.getItem() == null) {
-				return;
-			}
-			Dragboard dragboard = row.startDragAndDrop(TransferMode.MOVE);
-			ClipboardContent content = new ClipboardContent();
-			content.put(TaskController.format, row.getItem());
-			dragboard.setContent(content);
-			event.consume();
-		});
-
-		row.setOnDragOver(event -> {
-			if (event.getGestureSource() != row
-					&& event.getDragboard().hasContent(TaskController.format)) {
-				event.acceptTransferModes(TransferMode.MOVE);
-				event.consume();
-			}
-		});
-
-		row.setOnDragEntered(event -> {
-			if (event.getGestureSource() != row
-					&& event.getDragboard().hasContent(TaskController.format)) {
-				row.setOpacity(0.3);
-			}
-		});
-
-		row.setOnDragExited(event -> {
-			if (event.getGestureSource() != row
-					&& event.getDragboard().hasContent(TaskController.format)) {
-				row.setOpacity(1);
-			}
-		});
-
-		row.setOnDragDropped(event -> {
-
-			if (row.getItem() == null) {
-				return;
-			}
-
-			Dragboard db = event.getDragboard();
-			boolean success = false;
-
-			if (event.getDragboard().hasContent(TaskController.format)) {
-				ObservableList<Requirement> items = e1.getItems();
-				Requirement dragged = (Requirement) db.getContent(TaskController.format);
-
-				int draggedId = items.indexOf(dragged);
-				int thisId = items.indexOf(row.getItem());
-
-				e1.getItems().set(draggedId, row.getItem());
-				e1.getItems().set(thisId, dragged);
-
-				ArrayList<Requirement> reqs = assignment.getRequirements();
-				reqs.set(draggedId, row.getItem());
-				reqs.set(thisId, dragged);
-
-				success = true;
-				e1.refresh();
-			}
-			event.setDropCompleted(success);
-			event.consume();
-		});
-		return row;
-	}
-
 }
