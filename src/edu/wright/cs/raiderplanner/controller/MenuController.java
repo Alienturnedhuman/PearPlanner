db6cd5f2115974e4606fe64a852b5c7ed72ffe97
Andy <itsandybui@gmail.com>
2018-04-07 22:47:11 -0400

    Chat send button fixed and info input checks added
    An error message should now appear if no username/host info is provided.
    The send button should remain a fixed size as well.
    
    Resolves: #268

--- src/edu/wright/cs/raiderplanner/controller/MenuController.java
+++ src/edu/wright/cs/raiderplanner/controller/MenuController.java
@@ -966,8 +966,8 @@ public class MenuController implements Initializable {
 	}
 
 	/**
-	 * This will set the message area to uneditable and set the size for all the buttons This method
-	 * will also create padding between the textarea and the message area. and the send button.
+	 * This will set the message area to uneditable and set the size for all the buttons. The method
+	 * will also create padding between the text area, the message area, and the send button.
 	 */
 	public void createUserMessagePane() {
 		msgArea.setEditable(false);
@@ -978,6 +978,7 @@ public class MenuController implements Initializable {
 		userMessagePane.add(tfMessageToSend, 0, 0);
 		userMessagePane.add(spacingBox, 1, 0);
 		userMessagePane.add(sendButton, 2, 0);
+		sendButton.setMinWidth(100);
 	}
 
 	/**
@@ -995,18 +996,19 @@ public class MenuController implements Initializable {
 	/**
 	 * This will take in the action of when the submit button is pressed. The submit button is for
 	 * the chat window where the user inputs his or her information. If the user does not enter a
-	 * username then one will be appointed for them. Then at the very end the chat window will be
-	 * loaded.
+	 * username/hostname, an error will pop up notifying them to enter those values. Then at the
+	 * very end the chat window will be loaded.
 	 */
 	public void submitButtonAction() {
 		submitButton.setOnAction((ActionEvent exception1) -> {
-			if (tfName.getText().equals("")) {
-				tfName.setText("User" + Math.random());
-			} else {
+			if ((tfName.getText() != null && !(tfName.getText().equals("")))
+					&& (tfHost.getText() != null && !(tfHost.getText().equals("")))) {
 				userName = tfName.getText();
+				hostName = tfHost.getText();
+				loadChatWindow();
+			} else {
+				UiManager.displayError("Username and host are required.");
 			}
-			hostName = tfHost.getText();
-			loadChatWindow();
 		});
 	}
 
