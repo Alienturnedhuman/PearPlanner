390e8b58f79c493d949cf125c60ac4ca32fc3d6f
logankrause16 <logankrause16@gmail.com>
2018-09-23 12:44:11 -0400

    Exception Class - RaiderException #330

---
+++ src/edu/wright/cs/raiderplanner/util/RaiderException.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2018
+ *
+ *
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package edu.wright.cs.raiderplanner.util;
+
+/**
+ * @author LoganKrause
+ * @author CorbinMcGuire
+ * @author DanielHoward
+ */
+@SuppressWarnings("serial")
+public class RaiderException extends Exception {
+	
+	public RaiderException() {
+		// Magic stuff
+		getMessage();
+	}
+	
+	/*
+	 * Method that displays the actual message.
+	 */
+	public RaiderException(String message) {
+		super(message);
+		message = "Error, Please try again or seek help";
+	}
+	
+	// Able to assign specific message
+	public String getMessage() {
+		return super.getMessage();
+	}
+	
+	// Displays Error message and exception.
+	public RaiderException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+}
+
