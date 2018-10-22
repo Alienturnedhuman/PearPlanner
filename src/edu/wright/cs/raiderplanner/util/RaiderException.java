3e79d30dbf5db79229586ef28e686a7af2e71a3f
logankrause16 <logankrause16@gmail.com>
2018-10-21 20:32:14 -0400

    Database Creation And Dependency
    When the application is running, the SQL query will execute and the
    database table will be created.

--- src/edu/wright/cs/raiderplanner/util/RaiderException.java
+++ src/edu/wright/cs/raiderplanner/util/RaiderException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018
+ * Copyright (C) 2018 - Logan Krause, Corbin McGuire, Daniel Howard
  *
  *
  *
@@ -22,38 +22,45 @@
 package edu.wright.cs.raiderplanner.util;
 
 /**
+ * Custom exception class that displays more useful error messages to users and programmers.
  * @author LoganKrause
  * @author CorbinMcGuire
  * @author DanielHoward
  */
 @SuppressWarnings("serial")
 public class RaiderException extends Exception {
-	/*
-	 * Get the RaiderException
+	/**
+	 * Parameterless constructor.
 	 */
 	public RaiderException() {
 		getMessage();
 	}
-	/*
-	 * Method that displays the actual message.
+
+	/**
+	 * Message that displays the custom message.
+	 * @param message Helping message
 	 */
 	public RaiderException(String message) {
 		super(message);
-		message = "Error, Please try again or seek help";
+		message = "Error, Please try again or seek help.";
 	}
-	/*
-	 * (Javadoc)
+
+	/**
+	 * Returns the message to the user.
 	 * @see java.lang.Throwable#getMessage()
 	 * Able to assign specific message
 	 */
+	@Override
 	public String getMessage() {
 		return super.getMessage();
 	}
-	/*
+
+	/**
 	 * Displays Error message and exception.
+	 * @param message Receives the message passed by the thrown exception.
+	 * @param throwable Thrown exception.
 	 */
 	public RaiderException(String message, Throwable throwable) {
 		super(message, throwable);
 	}
 }
-
