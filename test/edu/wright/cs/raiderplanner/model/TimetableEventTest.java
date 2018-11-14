df6cea50e1f7ec207d52e6979bed81a01ed547d7
Adam Cone <adamarthurcone@gmail.com>
2018-11-14 12:42:22 -0500

    Added unit test code coverage for the TimetableEvent.java model.  Resolves #403

---
+++ test/edu/wright/cs/raiderplanner/model/TimetableEventTest.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2018 - Adam Cone
+ *
+ *
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package edu.wright.cs.raiderplanner.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Adam Cone 11/14/2018.
+ */
+public class TimetableEventTest {
+
+	String date;
+	Room room;
+	Person lecturer;
+	TimeTableEventType timeTableEventType;
+	int duration;
+	TimetableEvent timetableEventTest;
+
+	/**
+	 * This test case sets up the objects necessary for each tests.
+	 *
+	 * @throws Exception
+	 *             to handle when any of the required objects cannot be instantiated.
+	 */
+	@BeforeEach
+	public void setUp() throws Exception {
+		date = "11/14/2018";
+		room = new Room("152");
+		lecturer = new Person("Mr.", "Adam Cone", true, "adam@wright.edu");
+		timeTableEventType = new TimeTableEventType();
+		duration = 60;
+		timetableEventTest = new TimetableEvent(date, room, lecturer, timeTableEventType, duration);
+	}
+
+	/**
+	 * This test case attempts to retrieve room information.
+	 *
+	 * @throws Exception
+	 *             to handle when any of the required objects cannot be instantiated.
+	 */
+	@Test
+	public void getRoom() throws Exception {
+		assertEquals(this.room, timetableEventTest.getRoom());
+	}
+
+	/**
+	 * This test case attempts to retrieve lecturer information.
+	 *
+	 * @throws Exception
+	 *             to handle when any of the required objects cannot be instantiated.
+	 */
+	@Test
+	public void getLecturer() throws Exception {
+		assertEquals(this.lecturer, timetableEventTest.getLecturer());
+	}
+
+	/**
+	 * This test case attempts to retrieve timeTableEventType information.
+	 *
+	 * @throws Exception
+	 *             to handle when any of the required objects cannot be instantiated.
+	 */
+	@Test
+	public void getTimeTableEventType() throws Exception {
+		assertEquals(this.timeTableEventType, timetableEventTest.getTimeTableEventType());
+	}
+
+	/**
+	 * This test case attempts to set room information.
+	 *
+	 * @throws Exception
+	 *             to handle when any of the required objects cannot be instantiated.
+	 */
+	@Test
+	public void setRoom() throws Exception {
+		Room newRoom = new Room("300");
+		timetableEventTest.setRoom(newRoom);
+		assertEquals(newRoom, timetableEventTest.getRoom());
+	}
+
+	/**
+	 * This test case attempts to set lecturer information.
+	 *
+	 * @throws Exception
+	 *             to handle when any of the required objects cannot be instantiated.
+	 */
+	@Test
+	public void setLecturer() throws Exception {
+		Person newLecturer = new Person("Mr.", "Dam Done", true, "dam@wright.edu");
+		timetableEventTest.setLecturer(newLecturer);
+		assertEquals(newLecturer, timetableEventTest.getLecturer());
+	}
+
+	/**
+	 * This test case attempts to set timeTableEventType information.
+	 *
+	 * @throws Exception
+	 *             to handle when any of the required objects cannot be instantiated.
+	 */
+	@Test
+	public void setTimeTableEventType() throws Exception {
+		TimeTableEventType newTimeTableEventType = new TimeTableEventType();
+		timetableEventTest.setTimeTableEventType(newTimeTableEventType);
+		assertEquals(newTimeTableEventType, timetableEventTest.getTimeTableEventType());
+	}
+
+	/**
+	 * After each run, this removes all data to avoid interference with other tests.
+	 *
+	 * @throws Exception
+	 *             to handle when the Task objects cannot be accessed.
+	 */
+	@AfterEach
+	public void tearDown() throws Exception {
+		date = null;
+		room = null;
+		lecturer = null;
+		timeTableEventType = null;
+		duration = 0;
+		timetableEventTest = null;
+	}
+}
